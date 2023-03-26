2784cf008840f8b1406ec2f8a43dcd56ae7f3594
morsimantov <mor.goodsign@gmail.com>
2023-03-27 00:01:31 +0300

    edited parking lots screen

---
+++ lib/screens/parking_lots_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ParkingLotsScreen extends StatefulWidget {
+  const ParkingLotsScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ParkingLotsScreen> createState() => _ParkingLotsScreenState();
+}
+
+class _ParkingLotsScreenState extends State<ParkingLotsScreen> {
+  Positioned? _currentUserPosition;
+  double? distanceImMeter = 0.0;
+  // Data data = Data();
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+  
+}
